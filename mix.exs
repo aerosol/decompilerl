b5c21f6f173dee7081813bfb39d2742900d2e2f3
Adam Rutkowski <hq@mtod.org>
2016-10-16 22:59:39 +0200

    Remove cruft

--- mix.exs
+++ mix.exs
@@ -15,23 +15,7 @@ defmodule Decompilerl.Mixfile do
     [main_module: Decompilerl.CLI, embed_elixir: true]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    [applications: [:logger]]
-  end
+  def application, do: [applications: []]
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    []
-  end
+  defp deps, do: []
 end
