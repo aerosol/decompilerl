3b2db289cd9f334516604020a67fe81b442d92e4
Gordon Guthrie <gguthrie@basho.com>
2017-12-29 11:57:50 +0000

    Now offers a summary of the beam file and not the full thing
    See changes to the README for details

--- lib/decompilerl.ex
+++ lib/decompilerl.ex
@@ -1,9 +1,96 @@
 defmodule Decompilerl do
 
+  def summarise(module, device \\ :stdio) do
+    obtain_beam(module)
+    |> make_summary
+    |> format_summary
+    |> write_to(device)
+  end
+
   def decompile(module, device \\ :stdio) do
     obtain_beam(module)
     |> do_decompile
     |> write_to(device)
+  end
+
+  defp format_summary(map) do
+    %{:file       => file,
+      :module     => module,
+      :behaviours => behaviours,
+      :exports    => exports,
+      :functions  => functions} = map
+    lines = [
+      "File         : " <> file,
+      "Module       : " <> module,
+      format(behaviours, "Behaviours   : "),
+      format(exports,    "Exported Fns : "),
+      get_private_functions(functions, exports)
+      |>format("Private Fns  : ")
+    ]
+    Enum.join(List.flatten(lines), "\n")
+  end
+
+
+  defp format([], _prefix) do
+    []
+  end
+  defp format(behaviours, prefix) do
+    [h | t] = behaviours
+    lines = for x <- t, do: "               " <> x
+    [prefix <> h] ++ lines
+  end
+
+  defp get_private_functions(functions, exports) do
+    Enum.drop_while(functions, fn(x) -> Enum.member?(exports, x) end)
+  end
+
+  defp make_summary(beam_code) do
+    {:ok, {_, [abstract_code: {_, ac}]}} =
+      :beam_lib.chunks(beam_code, [:abstract_code])
+    {:tree, _, _, ast} = :erl_syntax.form_list(ac)
+    map = %{:file       => [],
+            :module     => [],
+            :behaviours => [],
+            :exports    => [],
+            :functions  => []}
+    Enum.reduce(ast, map, &process_ast/2)
+  end
+
+  defp process_ast({:attribute, _, :module, module}, map) do
+    Map.put(map, :module, Atom.to_string(module))
+  end
+
+  defp process_ast({:attribute, _, :file, {file, _}}, map) do
+    Map.put(map, :file, to_string(file))
+  end
+
+  defp process_ast({:attribute, _, :export, exports}, map) do
+    formatted_exports = for {func, arity} <- exports do
+      make_fn_declaration(func, arity)
+    end
+    append_value(map, :exports, formatted_exports)
+  end
+
+  defp process_ast({:attribute, _, :behaviour, behaviour}, map) do
+    append_value(map, :behaviours, [Atom.to_string(behaviour)])
+  end
+
+  defp process_ast({:function, _, func, arity, _body}, map) do
+    append_value(map, :functions, [make_fn_declaration(func, arity)])
+  end
+
+  ## dump the rest
+  defp process_ast(_clause, map) do
+    map
+  end
+
+  defp make_fn_declaration(func, arity) do
+    Atom.to_string(func) <> "/" <> Integer.to_string(arity)
+  end
+
+  defp append_value(map, key, valuelist) when is_list(valuelist) do
+    %{^key => values} = map
+    Map.put(map, key, values ++ valuelist)
   end
 
   defp obtain_beam(module) when is_atom(module) do
@@ -18,13 +105,13 @@ defmodule Decompilerl do
   defp do_decompile(beam_code) do
     {:ok, {_, [abstract_code: {_, ac}]}} =
       :beam_lib.chunks(beam_code, [:abstract_code])
-
     :erl_prettypr.format(:erl_syntax.form_list(ac))
   end
 
   defp write_to(code, :stdio) do
     IO.puts code
   end
+
   defp write_to(code, file_name) when is_binary(file_name) do
     {:ok, result} =
       File.open(file_name, [:write], fn(file) ->
--- lib/decompilerl/cli.ex
+++ lib/decompilerl/cli.ex
@@ -1,6 +1,6 @@
 defmodule Decompilerl.CLI do
-  @switches help: :boolean, output: :string
-  @aliases  h: :help, o: :output
+  @switches help: :boolean, output: :string, summary: :boolean
+  @aliases  h: :help, o: :output, s: :summary
 
   def main(argv) do
     argv
@@ -8,26 +8,42 @@ defmodule Decompilerl.CLI do
     |> process
   end
 
-  def parse_args(args) do
-    opts = 
+  defp parse_args(args) do
+    opts =
       OptionParser.parse(args, switches: @switches, aliases: @aliases)
 
-    case opts do
-      {[], [name], _} -> {name, :stdio}
-      {[output: output], [name], _} -> {name, output}
-      _ -> :help
+    {parsed, args, errors} = opts
+    case errors do
+      [] ->
+        case {Enum.sort(parsed), args} do
+          {[], [name]} ->
+            {:decompile, {name, :stdio}}
+          {[output: output], [name]} ->
+            {:decompile, {name, output}}
+          {[output: output, summary: true], [name]} ->
+            {:summarise, {name, output}}
+          {[summary: true], [name]} ->
+            {:summarise, {name, :stdio}}
+        end
+      _ ->
+        :help
     end
   end
 
-  def process(:help) do
+  defp process(:help) do
     IO.puts """
     Decompilerl
 
-    usage: decompierl <beam_file> [-o <erl_file> | --output=<erl_file>]
+    usage: decompierl <beam_file> [-o <erl_file> | --output=<erl_file> | -s | --summary]
     """
   end
 
-  def process({name, device}) do
+  defp process({:decompile, {name, device}}) do
     Decompilerl.decompile(name, device)
   end
+
+  defp process({:summarise, {name, device}}) do
+    Decompilerl.summarise(name, device)
+  end
+
 end
